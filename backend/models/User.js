a5ffe178f4ad31dd395f01cb1099892caa401e4d
kariebi <sorohmichael@gmail.com>
2023-10-11 19:57:58 -0400

    created userschema

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+    name: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    pasword: string
+})
+
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = UserModel
